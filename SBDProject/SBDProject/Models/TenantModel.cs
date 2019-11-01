4f18dc1c567ea24be71e6168ca2d4741f934c0f6
Adam <adam.sulimadolina@gmail.com>
2019-11-01 11:01:09 +0100

    Creating models

---
+++ SBDProject/SBDProject/Models/TenantModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SBDProject.Models
+{
+    public class TenantModel
+    {
+        [Required]
+        public int TenantID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public bool IsSmoking { get; set; }
+
+        public bool IsVege { get; set; }
+
+        public string Status { get; set; }
+
+        public int UserID { get; set; }
+
+        [ForeignKey("UserID")]
+        public virtual UserModel User { get; set; }
+    }
+}
---
+++ SBDProject/SBDProject/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SBDProject.Models
+{
+    public class UserModel
+    {
+        [Required]
+        public int UserID { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool Verified { get; set; }
+    }
+}
